35aea55b6f1a8a2bc24fbfce8a8a4ebe6ce71c57
shishir36982 <ranjanshishir24@gmail.com>
2016-11-06 16:45:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '1.html'));
+  res.sendFile(path.join(__dirname, 'ui', '1.HTML'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
